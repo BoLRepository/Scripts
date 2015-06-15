56abb57e9884f3a19e9fc4afab73254ff06ad4ce
BoLRepository <rob.barrow93@gmail.com>
2015-06-15 03:31:17 +0100

    Update Rengar.lua

--- Rengar.lua
+++ Rengar.lua
@@ -5,7 +5,7 @@ function Debug(message) print("<font color=\"#FFFFFF\"><b>Rengar:</font> </b><fo
 
 function OnLoad() 
     local ToUpdate = {}
-    ToUpdate.Version = 0.04
+    ToUpdate.Version = 0.07
     ToUpdate.UseHttps = true
     ToUpdate.Host = "raw.githubusercontent.com"
     ToUpdate.VersionPath = "/BoLRepository/Scripts/master/Rengar.version"
